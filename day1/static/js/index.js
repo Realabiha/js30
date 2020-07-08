cb3a2b7472fbcde20fa0a8d9ec3e0fbb052b7756
Realabiha <m17386550687@163.com>
2020-07-08 22:33:35 +0800

    day7

--- day1/static/js/index.js
+++ day1/static/js/index.js
@@ -3,14 +3,24 @@
 window.addEventListener('keydown', handleKeyDown);
 window.addEventListener('keyup', handleKeyUp);
 const audios = document.querySelectorAll('audio');
+const show = document.querySelector('.show')
+const li = document.getElementsByTagName('li');
 
+const keys = [
+    {key: 'a', code: 65}, {key: 's', code: 83}, {key: 'd', code: 68}, {key: 'f', code: 70}, 
+    {key: 'g', code: 71},
+    {key: 'h', code: 72}, {key: 'j', code: 74}, {key: 'k', code: 75}, {key: 'l', code: 76}
+];
+let index = 0;
 
 
 
 
 function handleKeyDown(e){
-    console.log(e.keyCode);
-    playSound(e);   
+    const btn = document.querySelector(`div[data-key="${e.keyCode}"]`);
+    if(!btn) return;
+    playSound(btn);   
+    lightKey(e);
 }
 function handleKeyUp(e){
     const btn = document.querySelector(`div[data-key="${e.keyCode}"]`);
@@ -18,11 +28,10 @@ function handleKeyUp(e){
     btn.classList.remove('play')
     const key = btn.querySelector('span');
     key.classList.remove('light');
+    show.classList.remove('active');
 }
 
-function playSound(e){
-    const btn = document.querySelector(`div[data-key="${e.keyCode}"]`);
-    if(!btn) return;
+function playSound(btn){
     btn.classList.add('play');
     const key = btn.querySelector('span');
     key.classList.add('light');
@@ -32,12 +41,38 @@ function playSound(e){
     audio.play();
 }
 
-function createBtn(){
-    let random = Math.floor(Math.random()*9);
-    if(nums[random] == keyCode) random ++;
-    keyCode = nums[random];
-    const btn = document.querySelector(`div[data-key="${nums[random]}"]`);
-    if(!btn) return;
-    btn.classList.add('play');
+function lightKey(e){
+    console.log(e.key)
+    show.textContent = e.key.toUpperCase(); 
+    show.classList.add('active');
 }
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// function outter(){
+//     let a = 1;
+//     // function inner(){
+//     //     console.log(a);
+//     // }
+//     inner();
+// }
+// function inner(){
+//     console.log(a)
+// }
+// outter();
